10473a0ee2dc6e81b5209f63785b6c3a3e1095a1
tomengsanchez <mrtomy31@gmail.com>
2020-09-14 15:28:24 +0800

    Update ecosys-project.php

--- ecosys-project.php
+++ ecosys-project.php
@@ -5,7 +5,7 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Tomeng Sanchez
+ * Author:            Tomeng Sanchez POGI
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       my-basics-plugin
