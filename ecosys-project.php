4c66d03cffafe0b575dca81d8bd8366386f5ab2f
tomengsanchez <mrtomy31@gmail.com>
2020-09-15 15:24:32 +0800

    Update ecosys-project.php

--- ecosys-project.php
+++ ecosys-project.php
@@ -5,7 +5,7 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Tomeng Sanchez Pogi
+ * Author:            Tomeng Sanchez Napaka POGI
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       Ecosys Project Management for Ecosys Systems Only
