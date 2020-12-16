86a798870d3ea3e2730abb37f74d6c58d77a6936
tomengsanchez <mrtomy31@gmail.com>
2020-12-16 15:54:45 +0800

    Update activities.php

--- activities/activities.php
+++ activities/activities.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+
+
+SproutVideo_Autoloader::register();
+SproutVideo::$api_key = 'a86f6731ecf186664d228ce23fdfa47e';
+
+SproutVideo\Video::list_videos();
 ?>
 <div class='container'>
     <div class='row'>
