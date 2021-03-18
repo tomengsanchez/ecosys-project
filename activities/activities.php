8109d1475e0eb0ae23160b4d3a053d2c944f4908
tomengsanchez <mrtomy31@gmail.com>
2021-03-18 15:29:54 +0800

    Update activities.php

--- activities/activities.php
+++ activities/activities.php
@@ -3,9 +3,20 @@
 
 
 ?>
-<div class='container'>
-    <div class='row'>
-        
+<i>This module will add activities on this project</i>
+<div class='container-fluid border'>
+    <div class='row'>   
+        <div class="col">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class='nav-link 'href='#'>Dashboard</a></li>
+                <li class="nav-item"><a class='nav-link 'href='#'>Add Activity</a></li>
+                <li class="nav-item"><a class='nav-link ' href='#'>Report</a></li>
+                
+            </ul> 
+        </div>
+        <div class="col-8">
+            
+        </div>
     </div>
 </div>
 <?php
